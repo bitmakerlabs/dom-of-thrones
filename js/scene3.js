d6646ffba4c469222b475e542cd1552357cacf91
Sean Lerner <sean@smallcity.ca>
2017-07-07 15:30:48 -0400

    fix danerys spelling

---
+++ js/scene3.js
@@ -0,0 +1,35 @@
+function scene3() {
+
+  // Setup Stage
+  mySrc = desert.getAttribute('src')
+  stageImg = document.querySelector('#stage-img')
+  stageImg.setAttribute('src', mySrc)
+
+
+  // Setup Cast
+
+  // Character 1
+
+  // Character 2
+
+  // Setup Props
+
+  // Prop 1
+
+  // Prop 2
+
+  // Action!
+
+  // Stage direction 1
+
+  frame(function() {
+
+  })
+
+  // Stage direction 2
+
+  frame(function() {
+
+  })
+
+}
